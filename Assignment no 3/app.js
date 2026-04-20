458e31d725fa75255361248bb5b5eb7385ee4ba8
Saminaqureshi <saminaqureshii191@gmail.com>
2026-04-20 17:03:11 +0500

    Add JavaScript code for age and visit tracking

---
+++ Assignment no 3/app.js
@@ -0,0 +1,32 @@
+// question no 1
+
+var age = 51; // apni age likho
+alert("My age is: " + age);
+
+// qusestion no 2
+
+var visitCount = 5; // jitni dafa visit kiya (example)
+
+document.write("You have visited this site " + visitCount + " times");
+
+
+// question no 3
+
+var birthYear = 1975; // apna birth year likho
+
+document.write("My birth year is " + birthYear + "<br>");
+document.write("Data type of my declared variable is number");
+
+
+//question no 4
+
+var visitorName = "John Doe";
+var productTitle = "T-shirt";
+var quantity = 5;
+
+document.write(visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store");
+
+
+
+
+
