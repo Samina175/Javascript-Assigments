6576586bb34b857aec0a38e9ba99b3d92198d09c
samina@@255206 <saminaqureshii191@gmail.com>
2026-04-21 14:18:59 +0500

    done

---
+++ assignment no.4/app.js
@@ -0,0 +1,26 @@
+1//question no 1
+
+var name = "Ali", age = 20, city = "Karachi";
+
+//question no 2
+
+var userName;
+var_age;
+var $price;
+var variable;
+var_name;
+
+//question no3
+document.write("<h1> Rules for naming js variables </h1>")
+document.write("Variable names can only contain letters, numbers, $ and _.<br>")
+document.write("For example $my_1stVariable<br><br>");
+document.write("Variables must begin with a letter, $ or _.<br>");
+document.write("For example $name, _name or name<br><br>");
+
+document.write("Variable names are case sensitive.<br><br>");
+
+document.write("Variable names should not be JS reserved keywords.");
+
+
+
+
